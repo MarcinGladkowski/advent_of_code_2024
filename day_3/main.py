2917eadf6d9438c0616727031ffc884aa7731a40
Marcin Gladkowski <marcingladkowski@gmail.com>
2024-12-04 21:12:22 +0100

    day_3(part_2): implemented - wrong result for now

--- day_3/main.py
+++ day_3/main.py
@@ -5,6 +5,9 @@ from input_loader import load_input, load_test_intput
 def filter_expressions(input_data: str):
     return re.findall(re.compile(r"mul\(\d+,\d+\)"), input_data)
 
+def filter_expressions_with_do(input_data: str):
+    return re.findall(re.compile(r"mul\(\d+,\d+\)|do\(\)|don't\(\)"), input_data)
+
 def calculate_expressions(expressions: list[str]):
 
     result = 0
@@ -14,13 +17,43 @@ def calculate_expressions(expressions: list[str]):
 
     return result
 
+def calculate_expressions_with_do_mode(expressions: list[str]):
+
+    result = 0
+    do_mode = True
+    for expression in expressions:
+
+        if expression == "don't()":
+            do_mode = False
+            continue
+
+        if expression == "do()":
+            do_mode = True
+            continue
+
+        if do_mode is False:
+            continue
+
+        numbers = re.findall(r"\d+", expression)
+        result += (int(numbers[0]) * int(numbers[1]))
+        do_mode = True
+
+    return result
 
 def run(input_data: str):
     expressions = filter_expressions(input_data)
     return calculate_expressions(expressions)
+
+def run_with_do_mode(input_data: str):
+    expressions = filter_expressions_with_do(input_data)
+    return calculate_expressions_with_do_mode(expressions)
 
 test_data_part_1_result = run(load_test_intput()[0])
 assert test_data_part_1_result == 161
 
 # part_1
 data_part_1_result = sum([run(x) for x in load_input()]) # 174336360
+# part_2
+data_part_2_result = sum([run_with_do_mode(x) for x in load_input()]) # 174336360
+# 98826679 to high
+print(data_part_2_result)
--- day_3/test_main.py
+++ day_3/test_main.py
@@ -1,4 +1,5 @@
-from day_3.main import filter_expressions, calculate_expressions
+from day_3.main import filter_expressions, calculate_expressions, filter_expressions_with_do, \
+    calculate_expressions_with_do_mode
 
 
 def test_filter_expressions():
@@ -10,3 +11,13 @@ def test_filter_expressions():
 
 def test_calculate():
     assert calculate_expressions(["mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"]) == 161
+
+def test_filter_expressions_with_do():
+    assert filter_expressions_with_do(
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    ) == ["mul(2,4)", "don't()", "mul(5,5)", "mul(11,8)", "do()", "mul(8,5)"]
+
+def test_calculate_with_do_mode():
+    assert calculate_expressions_with_do_mode(
+        ["mul(2,4)", "don't()", "mul(5,5)", "mul(11,8)", "do()", "mul(8,5)"]
+    ) == 48
