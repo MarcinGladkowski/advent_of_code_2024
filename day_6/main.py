948788d1d32926ae495b8d699fe2c0483bb7b9ec
Marcin Gladkowski <marcingladkowski@gmail.com>
2024-12-11 13:58:03 +0100

    day_6(part_1): simple movement recognition without obstacles

--- day_6/main.py
+++ day_6/main.py
@@ -12,17 +12,38 @@ class GuardWalker:
     def __init__(self, lab_map: list[list[str]]) -> None:
         self.lab_map = lab_map
         self.cursor = self.initial_position()
+        self.steps = 0
 
     def initial_position(self):
         for row_index, row in enumerate(self.lab_map):
             for col_index, col in enumerate(row):
                 if col in Move:
-                    return row_index, col_index
+                    return [row_index, col_index]
 
         raise RuntimeError('initial position of Guard not found')
 
-    def __next__(self):
-        pass
+    def __next__(self) -> "GuardWalker":
 
-    def step(self):
-        pass
+        direction = self.lab_map[self.cursor[0]][self.cursor[1]]
+
+        if Move.UP.value == direction:
+            self.cursor[0] -= 1
+            self.steps += 1
+            return self
+
+        if Move.DOWN.value == direction:
+            self.cursor[0] += 1
+            self.steps += 1
+            return self
+
+        if Move.LEFT.value == direction:
+            self.cursor[1] -= 1
+            self.steps += 1
+            return self
+
+        if Move.RIGHT.value == direction:
+            self.cursor[1] += 1
+            self.steps += 1
+            return self
+
+        return self
--- day_6/test_main.py
+++ day_6/test_main.py
@@ -2,30 +2,45 @@ from day_6.main import GuardWalker
 
 
 def test_found_initial_position():
-
     test_input = [
         ['.','.','.'],
         ['.','^','.'],
         ['.','.','.'],
     ]
-
-    assert (1,1) == GuardWalker(test_input).initial_position()
-
+    assert [1,1] == GuardWalker(test_input).initial_position()
 
 def test_move_up():
-    test_input = [
+    walker = next(GuardWalker([
         ['.','.','.'],
         ['.','^','.'],
         ['.','.','.'],
-    ]
-
-    assert True
+    ]))
+    assert walker.cursor == [0, 1]
+    assert walker.steps == 1
 
 def test_move_down():
-    pass
+    walker = next(GuardWalker([
+        ['.','.','.'],
+        ['.','v','.'],
+        ['.','.','.'],
+    ]))
+    assert walker.cursor == [2, 1]
+    assert walker.steps == 1
 
 def test_move_left():
-    pass
+    walker = next(GuardWalker([
+        ['.','.','.'],
+        ['.','<','.'],
+        ['.','.','.'],
+    ]))
+    assert walker.cursor == [1, 0]
+    assert walker.steps == 1
 
 def test_move_right():
-    pass
+    walker = next(GuardWalker([
+        ['.','.','.'],
+        ['.','>','.'],
+        ['.','.','.'],
+    ]))
+    assert walker.cursor == [1, 2]
+    assert walker.steps == 1
