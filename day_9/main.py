5665223ebbd7afd2aaea1dff4e46e26a96173893
MarcinGladkowski <marcingladkowski@gmail.com>
2024-12-18 22:57:31 +0100

    day_9(part_1): first disk map generation

---
+++ day_9/main.py
@@ -0,0 +1,18 @@
+def decode(raw_input: str):
+
+    decoded = []
+
+    for index, element in enumerate(raw_input):
+        if index == 0:
+            decoded.append(index)
+            continue
+        if index % 2 != 0:
+            decoded.extend([None for _ in range((int(element)))])
+            continue
+
+        decoded.extend([int(index / 2) for _ in range((int(element)))])
+
+    return decoded
+
+def rearrange(disk_map: list[int|None]) -> list[int|None]:
+    return disk_map
---
+++ day_9/test_main.py
@@ -0,0 +1,15 @@
+from day_9.main import decode, rearrange
+
+
+def test_decode():
+    assert decode('12345') == [0, None, None, 1, 1, 1, None, None, None, None, 2, 2, 2, 2, 2]
+
+
+def test_rearrange():
+    assert rearrange(
+        [0, None, None, 1, 1, 1, None, None, None, None, 2, 2, 2, 2, 2]
+    ) == [0, 2, 2, 1, 1, 1, 2, 2, 2, None, None, None, None, None, None]
+
+
+def test_calculate_checksum_on_basic():
+    assert sum([index * int(element) for index, element in enumerate('0099811188827773336446555566')]) == 1928
