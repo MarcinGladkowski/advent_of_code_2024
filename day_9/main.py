ac59716af316d0fe04bc4d0e22eae9d9ac720b2f
MarcinGladkowski <marcingladkowski@gmail.com>
2024-12-19 22:38:32 +0100

    day_9(part_2): working on solution

--- day_9/main.py
+++ day_9/main.py
@@ -1,4 +1,6 @@
 from copy import deepcopy
+from pprint import pprint
+
 
 def decode(raw_input: str):
     decoded = []
@@ -45,19 +47,59 @@ def rearrange(disk_map: list[int | None]) -> list[int]:
 
     return disk_map
 
+
+def is_free_slot(disk_element: list[None|int]) -> bool:
+    return all(list(map(lambda x: x is None, disk_element)))
+
+def is_allowed_to_apply(disk_element: list[None|int], candidate: list[int]) -> bool:
+    return len(disk_element) >= len(candidate)
+
 def rearrange_groups(disk_map: list[list[int|None]]) -> list[list[int]]:
     """
         Remember to split if group is smaller than space
 
         Not enumerate by disk_map but from end not None groups
     """
-    reverse_group_iterator = list(filter(lambda x: None not in x, deepcopy(disk_map)))
+    reverse_group_iterator = list(deepcopy(disk_map))
     reverse_group_iterator.reverse()
 
     for index, element in enumerate(reverse_group_iterator):
-        print(element)
+        if is_free_slot(element):
+            continue
 
-    return []
+        for disk_index, disk_element in enumerate(disk_map):
+            if is_free_slot(disk_element) and is_allowed_to_apply(disk_element, element):
+                """how to apply, needs split up"""
+                disk_allocate_space(element, disk_map, disk_index)
+                #deallocate_used_element(disk_map, len(disk_map)-index-1)
+                break
+
+    return disk_map
+
+def disk_allocate_space(disk_element: list[int], disk_map: list, disk_index: int) -> None:
+    """
+    Can allocate partially
+    e.g. 2 from 3 elements
+    """
+    if len(disk_element) == len(disk_map[disk_index]):
+        disk_map.insert(disk_index, disk_element)
+        disk_map.pop(disk_index + 1)
+        """allocate moved space!"""
+        return None
+
+    """
+        Split block, fill rest of block as [None**]
+    """
+    rest_of_disk_map = len(disk_map[disk_index]) - len(disk_element)
+    free_to_allocate = [None for _ in range(rest_of_disk_map)]
+    disk_map.insert(disk_index, free_to_allocate)
+    disk_map.insert(disk_index, disk_element)
+    disk_map.pop(disk_index+2)
+
+def deallocate_used_element(disk_map: list, disk_index: int) -> None:
+    pprint(disk_map)
+    pprint(disk_index)
+    disk_map.pop(disk_index)
 
 
 def is_free_space_on_right_side(disk_map: list[int]) -> bool:
--- day_9/test_main.py
+++ day_9/test_main.py
@@ -1,9 +1,10 @@
 from day_9.main import decode, rearrange, is_free_space_on_right_side, calculate_hash, decode_to_groups, \
-    rearrange_groups
+    rearrange_groups, is_free_slot
 
 
 def test_decode():
     assert decode('12345') == [0, None, None, 1, 1, 1, None, None, None, None, 2, 2, 2, 2, 2]
+
 
 def test_rearrange():
     assert rearrange(
@@ -11,13 +12,16 @@ def test_rearrange():
     ) == [0, 2, 2, 1, 1, 1, 2, 2, 2]
 
     assert rearrange(
-        [0,0, None, None, None, 1,1,1, None, None, None, 2, None, None, None, 3,3,3, None, 4,4, None, 5,5,5,5, None, 6,6,6,6, None,
-         7,7,7, None, 8,8,8,8,9,9]
+        [0, 0, None, None, None, 1, 1, 1, None, None, None, 2, None, None, None, 3, 3, 3, None, 4, 4, None, 5, 5, 5, 5,
+         None, 6, 6, 6, 6, None,
+         7, 7, 7, None, 8, 8, 8, 8, 9, 9]
     ) == [0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4, 4, 6, 5, 5, 5, 5, 6, 6]
+
 
 def test_is_finished_rearrange():
     assert is_free_space_on_right_side([0, 2, 2, 1, 1, 1, None, None, None, None, 2, 2, 2, 2, 2]) == False
     assert is_free_space_on_right_side([0, 2, 2, 1, 1, 1, None, None, None, None])
+
 
 def test_calculate_checksum_on_basic():
     assert sum([index * int(element) for index, element in enumerate('0099811188827773336446555566')]) == 1928
@@ -27,15 +31,25 @@ def test_calculate_checksum_on_basic():
          8, 8, 8, 0, 0]
     ) == 2858
 
+
 def test_decode_to_groups():
     assert decode_to_groups('2333133121414131402') == [
-        [0,0],[None, None, None],[1,1,1],[None, None, None],[2],[None, None, None],[3,3,3],[None],[4,4],[None],[5,5,5,5],[None],[6,6,6,6],[None],[7,7,7],[None],[8,8,8,8],[9,9]
+        [0, 0], [None, None, None], [1, 1, 1], [None, None, None], [2], [None, None, None], [3, 3, 3], [None], [4, 4],
+        [None], [5, 5, 5, 5], [None], [6, 6, 6, 6], [None], [7, 7, 7], [None], [8, 8, 8, 8], [9, 9]
     ]
+
+def test_is_free_slot():
+    assert is_free_slot([None, None, None])
+    assert is_free_slot([0, 0, 0]) == False
 
 def test_rearrange_with_groups():
     assert rearrange_groups(
-        [[0, 0], [None, None, None], [1, 1, 1], [None, None, None], [2], [None, None, None], [3, 3, 3], [None], [4, 4],
-        [None], [5, 5, 5, 5], [None], [6, 6, 6, 6], [None], [7, 7, 7], [None], [8, 8, 8, 8], [9, 9]]
+        [
+            [0, 0], [None, None, None], [1, 1, 1], [None, None, None], [2], [None, None, None], [3, 3, 3], [None],
+            [4, 4],
+            [None], [5, 5, 5, 5], [None], [6, 6, 6, 6], [None], [7, 7, 7], [None], [8, 8, 8, 8], [9, 9]
+        ]
     ) == [
-        [0,0],[9,9],[2],[1,1,1],[7,7,7],[None],[4,4],[None],[3,3,3],[None, None, None, None],[5,5,5,5],[None],[6,6,6,6],[None, None, None, None, None],[8,8,8,8],[None, None]
-    ]
+               [0, 0], [9, 9], [2], [1, 1, 1], [7, 7, 7], [None], [4, 4], [None], [3, 3, 3], [None, None, None, None],
+               [5, 5, 5, 5], [None], [6, 6, 6, 6], [None, None, None, None, None], [8, 8, 8, 8], [None, None]
+           ]
