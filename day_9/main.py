f586fff35179f6c968e58ce9c790851965bc5aad
MarcinGladkowski <marcingladkowski@gmail.com>
2024-12-19 06:45:47 +0100

    day_9(part_1): start working on rearranging

--- day_9/main.py
+++ day_9/main.py
@@ -15,4 +15,7 @@ def decode(raw_input: str):
     return decoded
 
 def rearrange(disk_map: list[int|None]) -> list[int|None]:
-    return disk_map
+    """
+    Rearranges the disk map
+    """
+    sorted(disk_map, key=lambda x: (x is None, x))
--- day_9/test_main.py
+++ day_9/test_main.py
@@ -8,7 +8,7 @@ def test_decode():
 def test_rearrange():
     assert rearrange(
         [0, None, None, 1, 1, 1, None, None, None, None, 2, 2, 2, 2, 2]
-    ) == [0, 2, 2, 1, 1, 1, 2, 2, 2, None, None, None, None, None, None]
+    ) == [0, 1, 1, 1, 2, 2, 2, 2, 2, None, None, None, None, None, None]
 
 
 def test_calculate_checksum_on_basic():
