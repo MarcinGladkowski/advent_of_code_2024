706d37a0a9e4960a1427242f56664a696d5eda0d
Marcin Gladkowski <marcingladkowski@gmail.com>
2024-12-19 12:43:45 +0100

    day_9(part_1): rearranging number for test cases

--- day_9/main.py
+++ day_9/main.py
@@ -1,5 +1,7 @@
-def decode(raw_input: str):
+from tarfile import fully_trusted_filter
 
+
+def decode(raw_input: str):
     decoded = []
 
     for index, element in enumerate(raw_input):
@@ -14,8 +16,29 @@ def decode(raw_input: str):
 
     return decoded
 
-def rearrange(disk_map: list[int|None]) -> list[int|None]:
+
+def rearrange(disk_map: list[int | None]) -> list[int]:
     """
     Rearranges the disk map
     """
-    sorted(disk_map, key=lambda x: (x is None, x))
+    for index, element in enumerate(disk_map):
+
+        if is_free_space_on_right_side(disk_map):
+            return list(filter(lambda x: x is not None, disk_map))
+
+        if element is None:
+            disk_map[index] = get_from_right(disk_map)
+            disk_map.insert(len(disk_map), None)
+
+    return disk_map
+
+def is_free_space_on_right_side(disk_map: list[int]) -> bool:
+    """When all free spaces on right side we are finished operation of rearrange"""
+    return len(list(filter(lambda x: x is not None, disk_map))) - 1 < disk_map.index(None)
+
+def get_from_right(disk_map: list[int]) -> int:
+    while True:
+        last_element = disk_map.pop()
+
+        if last_element is not None:
+            return last_element
--- day_9/test_main.py
+++ day_9/test_main.py
@@ -1,15 +1,22 @@
-from day_9.main import decode, rearrange
+from day_9.main import decode, rearrange, is_free_space_on_right_side
 
 
 def test_decode():
     assert decode('12345') == [0, None, None, 1, 1, 1, None, None, None, None, 2, 2, 2, 2, 2]
 
-
 def test_rearrange():
     assert rearrange(
         [0, None, None, 1, 1, 1, None, None, None, None, 2, 2, 2, 2, 2]
-    ) == [0, 1, 1, 1, 2, 2, 2, 2, 2, None, None, None, None, None, None]
+    ) == [0, 2, 2, 1, 1, 1, 2, 2, 2]
 
+    assert rearrange(
+        [0,0, None, None, None, 1,1,1, None, None, None, 2, None, None, None, 3,3,3, None, 4,4, None, 5,5,5,5, None, 6,6,6,6, None,
+         7,7,7, None, 8,8,8,8,9,9]
+    ) == [0, 0, 9, 9, 8, 1, 1, 1, 8, 8, 8, 2, 7, 7, 7, 3, 3, 3, 6, 4, 4, 6, 5, 5, 5, 5, 6, 6]
+
+def test_is_finished_rearrange():
+    assert is_free_space_on_right_side([0, 2, 2, 1, 1, 1, None, None, None, None, 2, 2, 2, 2, 2]) == False
+    assert is_free_space_on_right_side([0, 2, 2, 1, 1, 1, None, None, None, None])
 
 def test_calculate_checksum_on_basic():
     assert sum([index * int(element) for index, element in enumerate('0099811188827773336446555566')]) == 1928
