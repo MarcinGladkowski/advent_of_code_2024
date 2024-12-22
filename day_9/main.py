c5b1eaa8318bcce07b1de78311ec4752a97c4820
MarcinGladkowski <marcingladkowski@gmail.com>
2024-12-22 22:10:49 +0100

    day_9(part_2): start solution on simple list or string

--- day_9/main.py
+++ day_9/main.py
@@ -1,3 +1,6 @@
+import re
+
+
 def decode(raw_input: str):
     decoded = []
 
@@ -9,6 +12,7 @@ def decode(raw_input: str):
         decoded.extend([int(index / 2) for _ in range((int(element)))])
 
     return decoded
+
 
 def rearrange(disk_map: list[int | None]) -> list[int]:
     """
@@ -26,15 +30,18 @@ def rearrange(disk_map: list[int | None]) -> list[int]:
     return disk_map
 
 
-def is_allowed_to_apply(disk_element: list[None|int], candidate: list[int]) -> bool:
+def is_allowed_to_apply(disk_element: list[None | int], candidate: list[int]) -> bool:
     return len(disk_element) >= len(candidate)
+
 
 def create_free_slot(number_of_elements: int) -> list[None]:
     return [None for _ in range(number_of_elements)]
 
+
 def is_free_space_on_right_side(disk_map: list[int]) -> bool:
     """When all free spaces on right side we are finished operation of rearrange"""
     return len(list(filter(lambda x: x is not None, disk_map))) - 1 < disk_map.index(None)
+
 
 def get_from_right(disk_map: list[int]) -> int:
     while True:
@@ -55,30 +62,26 @@ def run(data_input: str) -> int:
     return calculate_hash(rearranged)
 
 
+def number_count(disk_map: list[int|None], number: str|int) -> int:
+    return len(list(filter(lambda x: x == int(number), disk_map)))
+
+def none_spots(disk_map: list[int|None]) -> list:
+    raw_map = ''.join(['.' if el is None else str(el) for el in disk_map])
+
+    return re.findall(r'\.+', raw_map)
 
 
+def full_blocks_rearrange(disk_map: list[int | None]) -> list[int]:
+    """
+        Rearranges the disk map for part II
+    """
+    for index, element in enumerate(disk_map):
+        if element is not None:
+            # get number of elements for number
+            # we can search elements and remove by number e.g. 13, 13, 13
+            number_len = number_count(disk_map, element)
+            # None sports
+            print(none_spots(disk_map))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return disk_map
--- day_9/test_main.py
+++ day_9/test_main.py
@@ -1,4 +1,5 @@
-from day_9.main import decode, rearrange, is_free_space_on_right_side, calculate_hash, is_free_slot
+from day_9.main import decode, rearrange, is_free_space_on_right_side, calculate_hash, full_blocks_rearrange
+
 
 def test_decode():
     assert decode('12345') == [0, None, None, 1, 1, 1, None, None, None, None, 2, 2, 2, 2, 2]
@@ -23,8 +24,16 @@ def test_is_finished_rearrange():
 
 def test_calculate_checksum_on_basic():
     assert sum([index * int(element) for index, element in enumerate('0099811188827773336446555566')]) == 1928
+
     """Replaces none with zeros"""
     assert calculate_hash(
         [0, 0, 9, 9, 2, 1, 1, 1, 7, 7, 7, 0, 4, 4, 0, 3, 3, 3, 0, 0, 0, 0, 5, 5, 5, 5, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 8,
          8, 8, 8, 0, 0]
     ) == 2858
+
+    """Check not filling empty spaces with zeroes to sum"""
+
+
+def test_full_rearrange():
+    assert (full_blocks_rearrange([0, 0, None, None, None, 1, 1, 1, 2, 2])
+            == [0, 0, 2, 2, None, 1, 1, 1])
